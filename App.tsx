080f2153ae3175ebeb375d2d1f107a29c6b1bdd3
Anderson Godoy <anderson.a.godoy@gmail.com>
2024-08-12 12:08:15 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,76 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TextInput, Image, Button, TouchableOpacity, TouchableHighlight, Switch } from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const [isEnabled, setIsEnabled] = useState(false)
+
+  const handleToggleIsEnabled = () => {
+    setIsEnabled(!isEnabled)
+  }
+
+  console.log(isEnabled)
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+
+    <View style={styles.container}>
+      <Text style={styles.welcome}>
+        Hello World
+      </Text>
+      <Image resizeMode='repeat' source={require('./assets/images/Fundo_de_tela.png')} style={{ width: 200, height: 200, alignSelf: 'center' }} />
+      <TextInput editable={true} style={styles.textInput} multiline textAlignVertical='top'
+        placeholder='telefone' placeholderTextColor={'#EEE'} />
+      <TextInput editable={true} style={styles.textInput} multiline textAlignVertical='top' placeholder='senha' placeholderTextColor={'#ccc'} />
+      <Button title='Click Me' onPress={() => alert('clicou')} />
+      <TouchableOpacity style={styles.button}>
+        <Text>Clica aqui Opacity</Text>
+      </TouchableOpacity>
+      <TouchableHighlight style={styles.button} onPress={() => alert('clicou')} activeOpacity={0.2} underlayColor='black' >
+        <Text>Clica Aqui HighLight</Text>
+      </TouchableHighlight>
+
+      <Switch value={isEnabled} onValueChange={handleToggleIsEnabled} trackColor={{
+        false: '#f00', true: 'blue'
+      }} thumbColor={'black'} />
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  welcome: {
+    color: '#ffffff',
+    backgroundColor: 'purple',
+    fontSize: 28,
+    padding: 8,
+    paddingHorizontal: 10,
+    marginVertical: 20,
+    borderRadius: 5
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  textInput: {
+    borderWidth: 1,
+    width: 230,
+    borderColor: 'blue',
+    borderRadius: 5,
+    padding: 8
   },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  button: {
+    marginTop: 20,
+    width: 230,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+    backgroundColor: 'pink',
+    borderRadius: 5,
+  }
+})
 
 export default App;
